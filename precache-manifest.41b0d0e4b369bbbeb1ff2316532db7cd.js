caf85997fa7c7e67513db83ba048406123be00bb
Shaikot <shaikot999@gmail.com>
2020-09-20 19:52:16 +0600

    Updates

---
+++ precache-manifest.41b0d0e4b369bbbeb1ff2316532db7cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd06bca4242f6fe0406fb074932adc2",
+    "url": "/Markdown-previewer/index.html"
+  },
+  {
+    "revision": "66540a0f634fa9f4210c",
+    "url": "/Markdown-previewer/static/css/main.baf768d2.chunk.css"
+  },
+  {
+    "revision": "660fb97cad4809c2d6f8",
+    "url": "/Markdown-previewer/static/js/2.37f01966.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Markdown-previewer/static/js/2.37f01966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66540a0f634fa9f4210c",
+    "url": "/Markdown-previewer/static/js/main.ea9b2d01.chunk.js"
+  },
+  {
+    "revision": "f3f7ffbd8f59c5f28f34",
+    "url": "/Markdown-previewer/static/js/runtime-main.ca3572a5.js"
+  }
+]);
